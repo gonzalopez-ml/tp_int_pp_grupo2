55cf1d2a65da941e854b0ce81697e6d2b9ed89dd
gonzalopez-ml <gonzalo.glopez@mercadolibre.com>
2023-11-05 12:38:40 -0300

    menu ongs

--- app/src/main/java/com/example/tp_integrador/MainActivityONG.java
+++ app/src/main/java/com/example/tp_integrador/MainActivityONG.java
@@ -1,18 +1,31 @@
 package com.example.tp_integrador;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 
+import com.example.tp_integrador.databinding.ActivityMainBinding;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import com.example.tp_integrador.databinding.ActivityMainOngBinding;
 
 public class MainActivityONG extends AppCompatActivity {
 
     private ActivityMainOngBinding binding;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
+    private AppBarConfiguration appBarConfiguration;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,13 +43,33 @@ public class MainActivityONG extends AppCompatActivity {
             }
         });
 
+        drawerLayout = findViewById(R.id.drawer_layout_ongs);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main_activity_ong);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph())
+                .setOpenableLayout(drawerLayout)
+                .build();
+
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_activity_o_n_g, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-
 }
