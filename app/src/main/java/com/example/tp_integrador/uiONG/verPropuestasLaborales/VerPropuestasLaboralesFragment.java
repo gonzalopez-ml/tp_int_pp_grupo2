cfdd7e7ceed16e0d78f41d9f4dc10925e62d0214
gabrielgiboudot <gabrielgiboudot@gmail.com>
2023-11-10 22:51:49 -0300

    + Editar y Eliminar, y refactor nombres.

--- app/src/main/java/com/example/tp_integrador/uiONG/verPropuestasLaborales/VerPropuestasLaboralesFragment.java
+++ app/src/main/java/com/example/tp_integrador/uiONG/verPropuestasLaborales/VerPropuestasLaboralesFragment.java
@@ -1,6 +1,9 @@
-package com.example.tp_integrador.uiONG.eliminarPropuestasLaborales;
+package com.example.tp_integrador.uiONG.verPropuestasLaborales;
 
 import androidx.lifecycle.ViewModelProvider;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -19,23 +22,23 @@ import java.util.List;
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
-public class EliminarPropuestasLaboralesFragment extends Fragment implements ProyectoEliminarAdapter.OnItemClickListener,ProyectoEliminarAdapter.OnEditItemClickListener {
+public class VerPropuestasLaboralesFragment extends Fragment implements ProyectoVerAdapter.OnItemClickListener, ProyectoVerAdapter.OnEditItemClickListener {
 
-    private EliminarPropuestasLaboralesViewModel mViewModel;
+    private VerPropuestasLaboralesViewModel mViewModel;
     private RecyclerView recyclerView;
-    private ProyectoEliminarAdapter proyectoEliminarAdapter;
+    private ProyectoVerAdapter proyectoVerAdapter;
 
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView =  inflater.inflate(R.layout.fragment_eliminar_propuestas_laborales, container, false);
-        mViewModel = new ViewModelProvider(this).get(EliminarPropuestasLaboralesViewModel.class);
+        View rootView =  inflater.inflate(R.layout.fragment_ver_propuestas_laborales, container, false);
+        mViewModel = new ViewModelProvider(this).get(VerPropuestasLaboralesViewModel.class);
 
         recyclerView = rootView.findViewById(R.id.rvEliminarProyectos);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        proyectoEliminarAdapter = new ProyectoEliminarAdapter(this,this);
-        recyclerView.setAdapter(proyectoEliminarAdapter);
+        proyectoVerAdapter = new ProyectoVerAdapter(this,this);
+        recyclerView.setAdapter(proyectoVerAdapter);
 
         //Se manda 1 pero aca va el id de la ONG logueada:
         mViewModel.getAllProjectsById(1).observe(getViewLifecycleOwner(),proyectos -> {
@@ -46,23 +49,47 @@ public class EliminarPropuestasLaboralesFragment extends Fragment implements Pro
 
 
     private void updateUIWithProyectos(List<Proyecto> proyectos) {
-        proyectoEliminarAdapter.setProjects(proyectos);
+        proyectoVerAdapter.setProjects(proyectos);
     }
     @Override
     public void onItemClick(Proyecto proyecto) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+        builder.setTitle("Confirmar Eliminación");
+        builder.setMessage("¿Está seguro que desea eliminar este proyecto?");
+
+        builder.setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Si el usuario hace clic en "Sí", procede con la eliminación del proyecto
+                deleteProyecto(proyecto);
+            }
+        });
+
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Si el usuario hace clic en "No", cierra el diálogo sin hacer nada
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+    }
+
+    private void deleteProyecto(Proyecto proyecto) {
         Boolean isProyectoDelete = mViewModel.deleteProjectLiveData(proyecto.getIdProyecto());
 
-        if(isProyectoDelete){
-            Toast.makeText(requireContext(),"Se elimino el proyecto con exito!",Toast.LENGTH_SHORT).show();
+        if (isProyectoDelete) {
+            Toast.makeText(requireContext(), "Se eliminó el proyecto con éxito!", Toast.LENGTH_SHORT).show();
             // Volvemos a cargar los proyectos después de eliminar uno
             mViewModel.getAllProjectsById(1).observe(getViewLifecycleOwner(), proyectos -> {
                 updateUIWithProyectos(proyectos);
             });
-
-        }else{
-            Toast.makeText(requireContext(),"Error al eliminar el proyecto, intente nuevamente!",Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(requireContext(), "Error al eliminar el proyecto, inténtelo nuevamente!", Toast.LENGTH_SHORT).show();
         }
-
     }
 
     @Override
